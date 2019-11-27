31b4aa7c3d8662ce963e2de61c3cbc49a0da1676
ganyong <ganyongest@163.com>
2019-11-27 21:58:35 +0800

    fgbfb

--- src/axios/request.js
+++ src/axios/request.js
@@ -9,15 +9,6 @@ const aa = axios.create(
 );
 
 
-// 添加响应拦截器
-aa.interceptors.response.use(function (response) {
-  // 对响应数据做点什么
-  return response
-}, function (error) {
-  // 对响应错误做点什么
-  return Promise.reject(error)
-});
-
 
 
 export default aa;
